0f494ff7695b9737a63d77a266efc4ed3431492a
tpauldike <topman4loveworld@gmail.com>
2022-12-26 21:24:38 +0100

    Working with _putchar

---
+++ 2-putchar_function/0-putchar.c
@@ -0,0 +1,15 @@
+/* A program that prints +Solaj and a new line*/
+#include "header.h"
+
+int main(void)
+{
+	_putchar('+');
+	_putchar('S');
+	_putchar('o');
+	_putchar('l');
+	_putchar('a');
+	_putchar('j');
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 2-putchar_function/looping.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+int main(void)
+{
+	char Christmas[] = "_putchar\n";
+	int z;
+	
+	for (z = 0; z <= strlen(Christmas); z++) 
+		_putchar(Christmas[z]);
+
+	return (0);
+}

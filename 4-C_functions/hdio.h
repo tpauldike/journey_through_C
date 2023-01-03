c1a8279cfa164a63e2fc8e787c56b334e3993bd5
tpauldike <topman4loveworld@gmail.com>
2023-01-03 23:43:54 +0000

    header file

---
+++ 4-C_functions/hdio.h
@@ -0,0 +1,12 @@
+#ifndef _HDIO_
+#define _HDIO_
+
+#include <stdio.h>
+
+/*prototypes*/ //header file == main.h
+void addNum(int a, int b);
+void subtractNum(int a, int b);
+void multiplyInt(int a, int b);
+void divideInt(int a, int b);
+
+#endif

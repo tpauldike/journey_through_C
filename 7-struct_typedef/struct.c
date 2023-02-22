8a00e20eaad8edfcd9dead8300bde964107852c6
tpauldike <topman4loveworld@gmail.com>
2023-02-22 21:23:58 +0000

    just int or an array of int?

---
+++ 7-struct_typedef/struct.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+  unsigned int test;
+  int trial[10];
+
+  scanf("%d", &test);
+  //scanf("%d", trial);
+
+  printf("test is: %d\n", test);
+
+  //printf("trial is: %d", trial);
+  return (0);
+
+}

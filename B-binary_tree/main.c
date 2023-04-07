c1d109782a525c34827840f9c87521a29f686b92
tpauldike <topman4loveworld@gmail.com>
2023-04-07 13:25:31 +0100

    binary search tree

--- B-binary_tree/main.c
+++ B-binary_tree/main.c
@@ -10,12 +10,33 @@ typedef struct node {
 
 int main(void)
 {
-	btr *root;
+	struct node *root;
+	btr *nextl, *nextr;
 
 	root = malloc(sizeof(btr));
-	root 
+	root->data = 6;
+	root->parent = NULL;
+	root->left = NULL;
+	root->right = NULL;
 
+	nextl = malloc(sizeof(btr));
+	root->left = nextl;
+	nextl->data = 3;
+	nextl->parent = root;
+	nextl->left = NULL;
+	nextl->right = NULL;
+
+	nextr = malloc(sizeof(btr));
+	root->right = nextr;
+	nextr->data = 8;
+	nextr->parent = root;
+	nextr->left = NULL;
+	nextr->right = NULL;
 	
+	nextl = malloc(sizeof(btr));
+	root->left->left = nextl;
+	//nextl->parent-> = root;
+
 
 	return (0);
 }
